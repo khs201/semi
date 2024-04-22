e1caafea8680280c8cd91e83faf64a009b2425f1
khs <hanseog201@naver.com>
2024-04-22 09:36:52 +0900

    프로젝트 생성

---
+++ IkmyungBoard/src/test/java/edu/kh/ib/IkmyungBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kh.ib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IkmyungBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
