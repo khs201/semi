d53a68e4661f27466019155dae7ba30eca151380
khs <hanseog201@naver.com>
2024-04-24 11:46:10 +0900

    임시

--- IkmyungBoard/src/main/java/edu/kh/ib/login/model/mapper/loginMapper.java
+++ IkmyungBoard/src/main/java/edu/kh/ib/login/model/mapper/loginMapper.java
@@ -1,9 +1,8 @@
 package edu.kh.ib.login.model.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.transaction.annotation.Transactional;
 
-import edu.kh.ib.login.model.dto.Member;
+import edu.kh.ib.member.model.dto.Member;
 
 @Mapper
 public interface loginMapper {
