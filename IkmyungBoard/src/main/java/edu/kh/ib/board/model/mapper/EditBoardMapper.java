ff85cd1e6f8159f188fd4ce7060a3256b215f12b
khs <hanseog201@naver.com>
2024-04-26 16:41:17 +0900

    중간 세이브

--- IkmyungBoard/src/main/java/edu/kh/ib/board/model/mapper/EditBoardMapper.java
+++ IkmyungBoard/src/main/java/edu/kh/ib/board/model/mapper/EditBoardMapper.java
@@ -1,6 +1,7 @@
 package edu.kh.ib.board.model.mapper;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
@@ -21,6 +22,30 @@ public interface EditBoardMapper {
 	 * @return
 	 */
 	int insertUploadList(List<BoardImg> uploadList);
+
+	/** 게시글 부분(제목/내용) 수정
+	 * @param inputBoard
+	 * @return
+	 */
+	int boardUpdate(Board inputBoard);
+
+	/** 게시글 이미지 삭제
+	 * @param map
+	 * @return
+	 */
+	int deleteImage(Map<String, Object> map);
+
+	/** 이미지 수정
+	 * @param img
+	 * @return
+	 */
+	int updateImage(BoardImg img);
+
+	/** 게시글 이미지 삽입(1행)
+	 * @param img
+	 * @return
+	 */
+	int insertImage(BoardImg img);
 	
 	
 	
